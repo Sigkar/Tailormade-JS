f36c1e187b6e94582025555d0baf9039f7866200
Duncan Pierce <duncan@duncanpierce.com>
2019-04-25 22:59:49 -0400

    Updated configurations

--- test/test.js
+++ test/test.js
@@ -1,20 +1,2 @@
-
-// function test(hours, minutes, seconds, expected) {
-// 	now.setHours(hours);
-// 	now.setMinutes(minutes);
-// 	now.setSeconds(seconds);
-
-// 	assert.equal(howLongTillLunch(...lunchtime), expected);
-// 	console.log(`\u001B[32m✓\u001B[39m ${expected}`);
-// }
-
-// let lunchtime = [ 12, 30 ];
-// test(11, 30, 0, '1 hour');
-// test(10, 30, 0, '2 hours');
-// test(12, 25, 0, '5 minutes');
-// test(12, 29, 15, '45 seconds');
-// test(13, 30, 0, '23 hours');
-
-// // some of us like an early lunch
-// lunchtime = [ 11, 0 ];
-// test(10, 30, 0, '30 minutes');
+// Use jest if thou wish.
+// Unit tests mainly need to go on the code end.
